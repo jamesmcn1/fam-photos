67560b3ae7cdb803439a31e137b2632a119858f4
James McNamara <james@pensionbee.com>
2020-12-24 16:17:48 +0000

    git ignore node modules

--- node_modules/@material-ui/styled-engine/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from '@emotion/styled';
-export { default } from '@emotion/styled';
-export { ThemeContext } from '@emotion/react';
-export { default as StylesProvider } from './StylesProvider';
--- node_modules/@material-ui/unstyled/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { default as BadgeUnstyled } from './BadgeUnstyled';
-export * from './BadgeUnstyled';
-
-export { default as SliderUnstyled } from './SliderUnstyled';
-export * from './SliderUnstyled';
-
-export * from './utils';
