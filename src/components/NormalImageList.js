45366838560f4ef54de5577eebc2674813ef030f
James McNamara <james@pensionbee.com>
2020-12-27 20:06:29 +0000

    image width sorted

--- src/components/NormalImageList.js
+++ src/components/NormalImageList.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ImageList, ImageListItem } from '@material-ui/core';
 import { Image } from 'cloudinary-react';
 import PropTypes from 'prop-types';
+import useWindowDimensions from '../hooks/useWindowDimensions';
 
-class NormalImageList extends Component {
-
-  render() {
-    return (
-      <ImageList>
-      {this.props.photos.map((item, i) => (
-        <ImageListItem key={item.img}>
-          <Image
-            publicId={item.public_id}
-            fetch-format="auto"
-            quality="auto"
-            width="500"
-            key={i}
+function NormalImageList(props) {
+  const { width } = useWindowDimensions();
+  return (
+    <ImageList cols={1}>
+    {props.photos.map((item, i) => (
+      <ImageListItem key={item.img}>
+        <Image
+          publicId={item.public_id}
+          fetch-format="auto"
+          quality="auto"
+          crop="scale"
+          width={width - 20}
+          key={i}
+        />
+        {/*
+          <ImageListItemBar
+            title={item.title}
+            subtitle={<span>by: {item.author}</span>}
           />
-          {/*
-            <ImageListItemBar
-              title={item.title}
-              subtitle={<span>by: {item.author}</span>}
-            />
-            */}
-        </ImageListItem>
-      ))}
-      </ImageList>
-    );
-  }
+          */}
+      </ImageListItem>
+    ))}
+    </ImageList>
+  );
 }
 
 NormalImageList.propTypes = {
