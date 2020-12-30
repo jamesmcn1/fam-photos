ce542a46b487d3739e53e3ddd2fa3cb8bfb14f7d
James McNamara <james@pensionbee.com>
2020-12-30 11:22:30 +0000

    rearrange index

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ cloudinary.config({
 
 // This middleware informs the express application to serve our compiled React files
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, './build')));
 
   app.get('*', function (req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, './build', 'index.html'));
   });
 }
 
