03773c41615fd18d902ed7e0cb3831a86c2ae160
James McNamara <james@pensionbee.com>
2020-12-31 11:49:27 +0000

    yo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cors = require('cors');
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 console.log('Starting server');
 
